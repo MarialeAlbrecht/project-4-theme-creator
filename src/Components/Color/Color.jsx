15fdc3676ec73584bf5bcaecbad7225fac9c9779
Maria Alejandra Albrecht <mariale.albrecht@gmail.com>
2025-09-02 10:21:29 +0200

    added the condition of the delete button

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import "./Color.css";
 
-export default function Color({ color }) {
+export default function Color({ color, onDelete }) {
+  const [confirmDelete, setConfirmDelete] = useState(false);
   return (
     <div
       className="color-card"
@@ -12,6 +14,22 @@ export default function Color({ color }) {
       <h3 className="color-card-headline">{color.hex}</h3>
       <h4>{color.role}</h4>
       <p>contrast: {color.contrastText}</p>
+
+      {confirmDelete ? (
+        <>
+          <p className="card-color-highlight">Really delete?</p>
+          <button type="submit" onClick={() => setConfirmDelete}>
+            Cancel
+          </button>
+          <button type="submit" onClick={() => onDelete(color.id)}>
+            Delete
+          </button>
+        </>
+      ) : (
+        <button type="submit" onClick={() => setConfirmDelete(true)}>
+          Delete
+        </button>
+      )}
     </div>
   );
 }
